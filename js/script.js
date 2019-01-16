97511ce71b27adf7bd098ef07ecd2a584cfa1228
Saleem Ali <brynjolf23@gmail.com>
2019-01-15 23:40:11 -0400

    Revert "testing visibility"
    This reverts commit 83616afc4c6561925673573c13161ae4f891bdd7.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 console.log('hello meme!');
 
-//what a meme
-/*For paragraph comments*/
-var doCoolStuff = function () {
-    var currentClassName = document.getElementById('cool').className;
-    
-    if (currentClassName == 'cool'){
-        document.getElementById('cool').className = 'cool red';
-    }
-    else {
-        document.getElementById('cool').className = 'cool';
-    }
-}
-
 var sayMyName = function (name) {
     alert("My name a: " + name);
 }
